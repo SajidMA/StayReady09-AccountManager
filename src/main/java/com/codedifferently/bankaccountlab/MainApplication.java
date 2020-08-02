80544a6559492981c1ac6bc826f97befa2bad7c8
SajidMA <smahmed@aggies.ncat.edu>
2020-08-02 16:45:30 -0400

    Finished Lab9, ready for review

--- src/main/java/com/codedifferently/bankaccountlab/MainApplication.java
+++ src/main/java/com/codedifferently/bankaccountlab/MainApplication.java
@@ -1,31 +1,268 @@
 package com.codedifferently.bankaccountlab;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainApplication {
 
-    Scanner in = new Scanner(System.in);
-    int expectedAccountNumber = 3696;
-    double expectedFees = 12.0;
-    String expectedName = "Josh";
-    double expectedBalance = 100000.0;
-    String expectedPassword = "Sajid";
-    CheckingAccount checkingAccount = new CheckingAccount(expectedAccountNumber, expectedFees, expectedBalance, expectedPassword, expectedName);
+    private static ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+    private static int currentAccountIndex;
 
-    int expectedAccountNumber2 = 4753;
-    double expectedFees2 = 12.0;
-    String expectedCompany = "JP Morgan Chase";
-    int expectedTaxId = 10;
-    double expectedBalance2 = 200000.0;
-    String expectedPassword2 = "Sajid";
-    BusinessAccount businessAccount = new BusinessAccount(expectedAccountNumber2, expectedFees2, expectedBalance2, expectedPassword2, expectedCompany, expectedTaxId);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-    int expectedAccountNumber3 = 6390;
-    double expectedFees3 = 12.0;
-    String expectedName2 = "Josh";
-    double expectedRates = 8.0;
-    double expectedBalance3 = 300000.0;
-    String expectedPassword3 = "Sajid";
-    SavingsAccount savingsAccount = new SavingsAccount(expectedAccountNumber3, expectedFees3, expectedBalance3, expectedPassword3, expectedName2, expectedRates);
+        int expectedAccountNumber2 = 4753;
+        double expectedFees2 = 12.0;
+        String expectedCompany = "JP Morgan Chase";
+        int expectedTaxId = 10;
+        double expectedBalance2 = 200000.0;
+        String expectedPassword = "Sajid";
+        BusinessAccount businessAccount = new BusinessAccount(expectedAccountNumber2, expectedFees2, expectedBalance2, expectedPassword, expectedCompany, expectedTaxId);
 
+        int expectedAccountNumber = 3696;
+        double expectedFees = 12.0;
+        double expectedBalance = 100000.0;
+        String expectedName = "Josh";
+        String expectedPassword2 = "Sajid";
+        CheckingAccount checkingAccount = new CheckingAccount(expectedAccountNumber, expectedFees, expectedBalance, expectedPassword2, expectedName);
+
+        int expectedAccountNumber3 = 6390;
+        double expectedFees3 = 12.0;
+        String expectedName2 = "Josh";
+        double expectedRates = 8.0;
+        double expectedBalance3 = 300000.0;
+        String expectedPassword3 = "Sajid";
+        SavingsAccount savingsAccount = new SavingsAccount(expectedAccountNumber3, expectedFees3, expectedBalance3, expectedPassword3, expectedName2, expectedRates);
+
+        double savingAccountBalance = 300000.0, withdraw, deposit;
+        double checkingAccountBalance = 100000.0, withdraw2, deposit2;
+        double businessAccountBalance = 200000.0, withdraw3, deposit3;
+
+        System.out.println("Welcome to the ATM");
+        System.out.println("Choose 1 to create account");
+        System.out.println("Choose 2 to view account");
+        System.out.println("Choose 3 to delete account");
+        System.out.println("Choose 4 to withdraw from an account");
+        System.out.println("Choose 5 to deposit from an account");
+        System.out.println("Choose the operation you want to perform");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        switch (n) {
+            case 1:
+                createAccount();
+                break;
+
+            case 2:
+                viewAccount();
+                break;
+
+            case 3:
+                deleteAccount();
+                break;
+
+            case 4:
+                withdrawFromAccount();
+                break;
+
+            case 5:
+                depositIntoAccount();
+                break;
+        }
+    }
+
+    public static void createAccount() {
+        System.out.println("Create an account!");
+        System.out.println("Choose 1 to make a Checking Account");
+        System.out.println("Choose 2 to make a Savings Account");
+        System.out.println("Choose 3 to make a Business Account");
+        System.out.println("Choose 4 to EXIT");
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        switch (n) {
+            case 1:
+                createCheckingAccount();
+                break;
+            case 2:
+                createSavingsAccount();
+                break;
+            case 3:
+                createBusinessAccount();
+                break;
+            case 4:
+                System.out.println("Not an available selection, select again");
+                createAccount();
+                break;
+        }
+    }
+
+    public static void createCheckingAccount() {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the information below: ");
+        System.out.println("Enter your name");
+        String name = s.nextLine();
+
+        System.out.println("Create a password");
+        String password = s.nextLine();
+
+        System.out.println("Create your account number");
+        int accountNumber = s.nextInt();
+
+        System.out.println("The fees for this account will go as followed");
+        double fees = s.nextDouble();
+
+        System.out.println("Your new balance is");
+        double balance = s.nextDouble();
+
+        System.out.println("What will your first deposit be?");
+        double initalDeposit = s.nextDouble();
+        balance += initalDeposit;
+
+        CheckingAccount checkingAccount = new CheckingAccount(accountNumber, fees, balance, password, name);
+
+        accounts.add(checkingAccount);
+    }
+
+    public static void createSavingsAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = s.next();
+
+        System.out.println("Create your account number");
+        int accountNumber = s.nextInt();
+
+        System.out.println("Create a password");
+        String password = s.nextLine();
+
+        System.out.println("The fees for this account will go as followed");
+        double fees = s.nextDouble();
+
+        System.out.println("The rates for this account will go as followed");
+        double rates = s.nextDouble();
+
+        System.out.println("Your new balance is");
+        double balance = s.nextDouble();
+
+        System.out.println("What will your first deposit be?");
+        double initialDeposit = s.nextDouble();
+        balance += initialDeposit;
+
+        SavingsAccount savingsAccount = new SavingsAccount(accountNumber, fees, balance, password, name, rates);
+        accounts.add(savingsAccount);
+    }
+
+    public static void createBusinessAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Company Name");
+        String company = s.nextLine();
+
+        System.out.println("Create your account number");
+        int accountNumber = s.nextInt();
+
+        System.out.println("Create a password");
+        String password = s.nextLine();
+
+        System.out.println("What is your taxId");
+        int taxId = s.nextInt();
+
+        System.out.println("The fees for this account will go as followed");
+        double fees = s.nextDouble();
+
+        System.out.println("The balance for this account will go as followed");
+        double balance = s.nextDouble();
+
+        System.out.println("What will your first deposit be?");
+        double initialDeposit = s.nextDouble();
+        balance += initialDeposit;
+
+        BusinessAccount businessAccount = new BusinessAccount(accountNumber, fees, balance, password, company, taxId);
+        accounts.add(businessAccount);
+    }
+
+    public static void viewAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What's your accountNumber");
+        int accountNumber = s.nextInt();
+    }
+
+    public static void loginChoices() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome! Here is a list of options to chose from");
+
+        System.out.println("Choose 1 to deposit");
+        System.out.println("Choose 2 to withdraw");
+        System.out.println("Choose 3 to check balance");
+        System.out.println("Choose 4 to Delete Account");
+
+        int userChoice = s.nextInt();
+
+        //non plural
+        loginChoice(userChoice);
+    }
+
+    public static void loginChoice(int userChoice) {
+        switch (userChoice) {
+            case 1:
+                depositIntoAccount();
+                break;
+            case 2:
+                withdrawFromAccount();
+                break;
+            case 3:
+                accountBalance();
+                break;
+            case 4:
+                deleteAccount();
+                break;
+        }
+    }
+
+    public static void depositIntoAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter deposit amount");
+        double depositAmt = s.nextDouble();
+
+        //gets the most recent account in use, and deposits
+        accounts.get(currentAccountIndex).depositInBalance(depositAmt);
+
+        System.out.println("Your new balance is: " + accounts.get(currentAccountIndex).getBalance());
+        loginChoices();
+    }
+
+    public static void withdrawFromAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter withdraw amount");
+        double withdrawAmt = s.nextDouble();
+
+        //gets the most recent account in use, and deposits
+        if (withdrawAmt > 0) {
+            accounts.get(currentAccountIndex).withdrawFromBalance(withdrawAmt);
+            System.out.println("Your new balance is: " + accounts.get(currentAccountIndex).getBalance());
+        } else {
+            //returns the withdraw error message defined in the method
+            accounts.get(currentAccountIndex).withdrawFromBalance(withdrawAmt);
+        }
+        loginChoices();
+    }
+
+    public static void accountBalance() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Your balance is: " +
+                accounts.get(currentAccountIndex).getBalance());
+        loginChoices();
+    }
+
+    public static void deleteAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Are you sure you want to delete your account");
+        String input = s.next();
+
+        if (input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("Y")) {
+            accounts.remove(accounts.get(currentAccountIndex));
+            System.out.println("Account deleted");
+        }
+    }
 }
